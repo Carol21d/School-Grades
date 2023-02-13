e190a9a92540ed2e607d2fe36bcb8232a442a523
blake S <design.harls@icloud.com>
2023-02-13 23:31:28 +0100

    styles added

--- src/components/FormStudents.vue
+++ src/components/FormStudents.vue
@@ -1,25 +1,46 @@
-
-<script setup>
-
-</script>
+<script setup></script>
 <template>
   <div class="wrapper">
     <form @submit.prevent="addStudent()" class="wrapper__form">
-      <input type="text" placeholder="Name of Student">
-      <input type="text" placeholder="Subject">
-      <input type="number" placeholder="Score">
+      <input type="text" placeholder="Name of Student" />
+      <input type="text" placeholder="Subject" />
+      <input type="number" placeholder="Score" />
       <button type="submit">Add</button>
-
-
-    
-    
-    
     </form>
-
-
   </div>
-
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 80%;
+
+  .wrapper__form {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: aqua;
+    width: auto 0;
+    height: auto 0;
+
+    input {
+      display: flex;
+      margin: 1em 1em;
+      border: none;
+      font-size: 1.2em;
+      font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+        "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    }
+
+    button{
+      display: flex;
+      margin-right: 1em;
+    }
+  }
+}
 </style>
